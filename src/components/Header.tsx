1afbaa98248a4aacfee40d05227da3e99918dc9d
Mike Wajda <mewajda@gmail.com>
2022-12-23 14:45:39 -0600

    feat: added import/export functionality
    Export allows the user to save a json file to their machine
    
    Import you must copy and paste the profile json
    
    https://github.com/nixtus/cyclops/issues/3

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,11 +1,15 @@
-import { Button, Checkbox, Dropdown, Navbar } from 'flowbite-react';
-import React, { useContext } from 'react';
+import { Textarea, Button, Checkbox, Dropdown, Navbar, Modal, Alert } from 'flowbite-react';
+import React, { useContext, useState } from 'react';
 import { GlobalContext } from '../context/Global';
 import { ActionType } from '../context/Global/actions';
+import { parseImportSettings } from '../utils';
 
 export function Header() {
     const { state: globalState, dispatch } = useContext(GlobalContext);
     const { enabled: globalEnabled, profiles } = globalState || {};
+    const [showModal, setShowModal] = useState(false);
+    const [profileData, setProfileData] = useState<any>();
+    const [showError, setShowError] = useState(false);
 
     const handleGlobalEnable = () => {
         dispatch({
@@ -45,6 +49,66 @@ export function Header() {
             type: ActionType.DELETE_CURRENT_PROFILE
         });
     };
+
+    const exportProfiles = () => {
+        const profilesBlob = new Blob([JSON.stringify(globalState.profiles)], { type: 'application/json' });
+        const url = URL.createObjectURL(profilesBlob);
+
+        chrome.downloads.download({
+            url,
+            saveAs: true
+        });
+    };
+
+    const importProfiles = () => {
+        try {
+            const data = JSON.parse(profileData);
+            const result = parseImportSettings(data);
+
+            if (result.success) {
+                const updatedProfiles = [];
+
+                for (const currentProfile of profiles) {
+                    const found = result.data.find((x) => x.id === currentProfile.id);
+
+                    if (found) {
+                        updatedProfiles.push({
+                            ...currentProfile,
+                            ...found,
+                            currentlySelected: false
+                        });
+                    } else {
+                        updatedProfiles.push({ ...currentProfile, currentlySelected: false });
+                    }
+                }
+
+                for (const importProfile of result.data) {
+                    const found = updatedProfiles.find((x) => x.id === importProfile.id);
+                    if (!found) {
+                        updatedProfiles.push({ ...importProfile, currentlySelected: false });
+                    }
+                }
+                updatedProfiles[0].currentlySelected = true;
+
+                dispatch({
+                    type: ActionType.SET_PROFILES,
+                    payload: updatedProfiles
+                });
+
+                setShowError(false);
+                setShowModal(false);
+            } else {
+                setShowError(true);
+            }
+        } catch (error) {
+            setShowError(true);
+        }
+    };
+
+    const toggleModal = () => {
+        setShowModal(!showModal);
+    };
+
     return (
         <nav className="relative w-full flex flex-wrap items-center justify-between py-3 bg-gray-100 text-gray-500 hover:text-gray-700 focus:text-gray-700 shadow-lg">
             <div className="container-fluid w-full flex flex-wrap items-center justify-between px-6">
@@ -62,13 +126,46 @@ export function Header() {
                     </li>
                     <li className="nav-item px-2">
                         <Button.Group>
+                            {profiles?.length ? (
+                                <Dropdown label="Profiles" size="xs" gradientMonochrome="info">
+                                    {profiles
+                                        .sort((a, b) => {
+                                            const nameA = a.name.toUpperCase();
+                                            const nameB = b.name.toUpperCase();
+                                            if (nameA < nameB) {
+                                                return -1;
+                                            }
+                                            if (nameA > nameB) {
+                                                return 1;
+                                            }
+
+                                            return 0;
+                                        })
+                                        .map((profile, idx) => {
+                                            return (
+                                                <div key={idx} className="flex flex-wrap items-center">
+                                                    <Checkbox
+                                                        name="enabled"
+                                                        id={`${idx}enabled`}
+                                                        checked={profile.enabled}
+                                                        onChange={() => handleProfileEnableChange(profile.id)}
+                                                        disabled={!globalEnabled}
+                                                    />
+                                                    <Dropdown.Item onClick={() => handleSetProfile(profile.id)}>
+                                                        {profile.name}
+                                                    </Dropdown.Item>
+                                                </div>
+                                            );
+                                        })}
+                                </Dropdown>
+                            ) : null}
                             <Button
                                 size="xs"
                                 disabled={!globalEnabled}
                                 gradientMonochrome="info"
                                 onClick={handleAddProfile}
                             >
-                                Add Profile
+                                Add
                             </Button>
                             <Button
                                 size="xs"
@@ -76,7 +173,7 @@ export function Header() {
                                 disabled={!globalEnabled}
                                 gradientMonochrome="info"
                             >
-                                Duplicate Profile
+                                Duplicate
                             </Button>
                             <Button
                                 size="xs"
@@ -84,46 +181,52 @@ export function Header() {
                                 disabled={!globalEnabled}
                                 gradientMonochrome="info"
                             >
-                                Delete Profile
+                                Delete
                             </Button>
                         </Button.Group>
                     </li>
-                    {profiles?.length ? (
-                        <li className="nav-item px-2">
-                            <Dropdown label="Profiles" size="xs">
-                                {profiles
-                                    .sort((a, b) => {
-                                        const nameA = a.name.toUpperCase();
-                                        const nameB = b.name.toUpperCase();
-                                        if (nameA < nameB) {
-                                            return -1;
-                                        }
-                                        if (nameA > nameB) {
-                                            return 1;
-                                        }
-
-                                        return 0;
-                                    })
-                                    .map((profile, idx) => {
-                                        return (
-                                            <div key={idx} className="flex flex-wrap items-center">
-                                                <Checkbox
-                                                    name="enabled"
-                                                    id={`${idx}enabled`}
-                                                    checked={profile.enabled}
-                                                    onChange={() => handleProfileEnableChange(profile.id)}
-                                                    disabled={!globalEnabled}
-                                                />
-                                                <Dropdown.Item onClick={() => handleSetProfile(profile.id)}>
-                                                    {profile.name}
-                                                </Dropdown.Item>
-                                            </div>
-                                        );
-                                    })}
-                            </Dropdown>
-                        </li>
-                    ) : null}
+                    <li className="nav-item px-2">
+                        <Button.Group>
+                            <Button
+                                size="xs"
+                                onClick={exportProfiles}
+                                disabled={!globalEnabled}
+                                gradientMonochrome="info"
+                            >
+                                Export
+                            </Button>
+                            <Button size="xs" onClick={toggleModal} disabled={!globalEnabled} gradientMonochrome="info">
+                                Import
+                            </Button>
+                        </Button.Group>
+                    </li>
                 </ul>
+                <Modal show={showModal} onClose={toggleModal}>
+                    <Modal.Header>Import Profiles</Modal.Header>
+                    {showError ? (
+                        <Alert color="failure" onDismiss={() => setShowError(false)}>
+                            <span>
+                                <span className="font-medium">Invalid Input</span>
+                                The profile information you entered is not in the correct format
+                            </span>
+                        </Alert>
+                    ) : null}
+                    <Modal.Body>
+                        <div className="space-y-6">
+                            <Textarea
+                                value={profileData}
+                                onChange={(e) => setProfileData(e.target.value as any)}
+                                id="profiles"
+                                placeholder="Paste in profiles json..."
+                                required={true}
+                                rows={6}
+                            />
+                        </div>
+                    </Modal.Body>
+                    <Modal.Footer>
+                        <Button onClick={importProfiles}>Import</Button>
+                    </Modal.Footer>
+                </Modal>
             </div>
         </nav>
     );
--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,5 +1,28 @@
+import { z } from 'zod';
+
 export function getRandomId() {
     return Math.floor(Math.random() * 100_000_000);
 }
 
 export const CYCLOPS_SETTINGS_STORAGE_KEY = 'CYCLOPS_SETTINGS';
+
+const profileRequestHeaderSchema = z.object({
+    id: z.number(),
+    name: z.string(),
+    value: z.string(),
+    enabled: z.boolean()
+});
+
+const profileSchema = z.object({
+    id: z.number(),
+    name: z.string(),
+    requestHeaders: z.array(profileRequestHeaderSchema),
+    enabled: z.boolean(),
+    currentlySelected: z.boolean()
+});
+
+const profileArraySchema = z.array(profileSchema);
+
+export function parseImportSettings(profiles: unknown) {
+    return profileArraySchema.safeParse(profiles);
+}
